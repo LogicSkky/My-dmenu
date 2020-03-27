a5fe4c071e12b51dedb1172f280d4aa157bd64fe
LogicSkky <wangkffrank@qq.com>
2020-03-27 14:53:22 +0800

    changed the size of the font

--- config.h
+++ config.h
@@ -7,7 +7,7 @@ static int centered  = 1;                /* -c option; centers dmenu on screen *
 static int min_width = 500;              /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Source Code Pro:size=11"
+	"Source Code Pro:size=12"
 };
 static const char *prompt                = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
